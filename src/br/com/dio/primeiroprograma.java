9bdfdd3700fd4429069cc39691d9175e1a60eadf
sergioluizalmeida <chamandosergio@hotmail.com>
2022-01-28 21:26:51 -0300

    First Commit IntelliJ

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();         /* Quando a classe gato fica em evidÊncia ´´eo pq nao colocamos o import */
+        System.out.println(gato);
+
+
+
+       /* int a = 5;
+        int b = 3;
+        System.out.println("Hello World - 5 + 3 = " + (a+b));*/
+    }
+
+}
